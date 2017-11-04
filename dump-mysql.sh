a5cf937f5f60de8f7e7dc78d4f733b5a3e91fa23
leschard <leschard@github>
2017-11-05 00:46:52 +0100

    update dump sql

--- dump-mysql.sh
+++ dump-mysql.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-mysqldump -u root -p$MYSQL_ROOT_PASSWORD --all-databases  | bzip2 -c >  /srv/backup-$(date +%Y-%m-%d-%H.%M.%S).sql.bz2
+mysqldump -u root -p$(cat /run/secrets/db_pwd) --all-databases | gzip  > /srv/backup-$(date +%Y-%m-%d-%H.%M.%S).sql.gz
