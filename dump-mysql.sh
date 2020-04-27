a8bf562e4e08b20ae0a30b04814de852dcc4c205
Ludovic Eschard <ludovic@eschard.com>
2020-04-27 20:20:09 +0200

    fix: quote expressions

--- dump-mysql.sh
+++ dump-mysql.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-mysqldump -u root -p$(cat /run/secrets/db_pwd) --all-databases | gzip  > /srv/backup-$(date +%Y-%m-%d-%H.%M.%S).sql.gz
+mysqldump -u root -p"$(cat /run/secrets/db_pwd)" --all-databases | gzip  > /srv/backup-"$(date +%Y-%m-%d-%H.%M.%S)".sql.gz
